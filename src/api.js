b7c9a49163828fe46654ae2841a3275231565cff
kenil000 <57092884+kenil000@users.noreply.github.com>
2022-05-15 15:11:13 +0200

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,21 @@
+export const getComments = async () => {
+  return [];
+};
+
+export const createComment = async (text, parentId = null) => {
+  return {
+    id: Math.random().toString(36).substr(2, 9),
+    body: text,
+    parentId,
+    userId: "1",
+    createdAt: new Date().toISOString(),
+  };
+};
+
+export const updateComment = async (text) => {
+  return { text };
+};
+
+export const deleteComment = async () => {
+  return {};
+};
